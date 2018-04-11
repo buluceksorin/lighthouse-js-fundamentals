23f3f4589b3202f034b06cc81dd426b993992fad
Sorin <bulucek.sorin@gmail.com>
2018-04-11 00:06:25 +0000

    Adds looping exercises

--- hello.js
+++ hello.js
@@ -5,3 +5,31 @@ function sayHello(name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+
+function ageCalculator(){
+
+  var name = 'Suzie';
+  var name2 ='Jack';
+  var yearofBirth = 1983;
+  var yearofBirth2 = 2004;
+  var currentYear = 2015;
+  var currentYear2 = 2016;
+  var age = currentYear - yearofBirth;
+  var age2 = currentYear2 - yearofBirth2;
+
+  return name +  " is "  +  age + " years old " ;
+  return name2 +  " is "  +  age + " years old " ;
+
+}
+
+
+
+console.log(ageCalculator("Suzie", 1983, 2015));
+console.log(ageCalculator("Jack", 2004, 2016));
+
+
+
+
+
+
--- odd.js
+++ odd.js
@@ -8,3 +8,9 @@ console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
 
 
+var city = "Vancouver";
+var yearIncorporated = 1886;
+var currentYear = 2015;
+var age = currentYear - yearIncorporated;
+
+city + " is " + age + " years old.";
